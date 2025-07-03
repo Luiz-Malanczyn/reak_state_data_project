f023bf92d6519e49a943b5415ec2cdb99943445c
Luiz Eduardo Malanczyn de Oliveira <oliveiraluiz.com@hotmail.com>
2025-07-03 12:29:53 -0300

    feat: update Chaves na Mão scraper to improve ad parsing and data extraction

--- pipeline/extract/chavesnamao_scraper.py
+++ pipeline/extract/chavesnamao_scraper.py
@@ -6,7 +6,6 @@ class ChavesNaMaoScraper(BaseScraper):
         super().__init__("https://www.chavesnamao.com.br/imoveis-a-venda/pr-curitiba/")
 
     def get_ads(self, page):
-        # Scroll manual pra carregar tudo
         page_height = page.evaluate("document.body.scrollHeight")
         current_height = 500
 
@@ -16,36 +15,39 @@ class ChavesNaMaoScraper(BaseScraper):
             current_height += 500
 
         page.wait_for_timeout(2000)
-        return page.query_selector_all('div[data-template="list"].styles-module__ViVk2q__card.card-module__cvK-Xa__card')
+        return page.query_selector_all('div[data-template="list"].styles-module__saqrOW__card.card-module__1awNxG__card')
 
     def parse_ad(self, ad, page):
         def get_text(el):
             return el.inner_text().strip() if el else ""
 
-        titulo_el = ad.query_selector("h2.olx-text.olx-text--body-large")
+        titulo_el = ad.query_selector("h2.styles-module__5TXV2W__heading2 styles-module__saqrOW__title.style-module__PkTDxW__contentTitle")
         titulo = get_text(titulo_el) or "Título não encontrado"
 
-        infos = ad.query_selector('span.card-module__cvK-Xa__cardContent')
+        infos = ad.query_selector('span.card-module__1awNxG__cardContent')
 
         preco = self._extract_val(get_text(infos.query_selector('b')), r"([\d\.,]+)")
-        cond = self._extract_val(get_text(infos.query_selector('small[aria-label="Condominio"]')), r"R\$ ([\d\.,]+)")
-        iptu = self._extract_val(get_text(infos.query_selector('small[aria-label="IPTU"]')), r"R\$ ([\d\.,]+)")
+        cond = self._extract_val(get_text(infos.query_selector('span[aria-label="Condominio"]')), r"R\$ ([\d\.,]+)")
+        iptu = self._extract_val(get_text(infos.query_selector('span[aria-label="IPTU"]')), r"R\$ ([\d\.,]+)")
 
-        detalhes_container = infos.query_selector("span.style-module__Yo5w-q__list ")
-        detalhes = detalhes_container.query_selector_all("p.styles-module__aBT18q__body2") if detalhes_container else []
+        detalhes_container = infos.query_selector("span.style-module__PkTDxW__list")
+        detalhes = detalhes_container.query_selector_all("p.styles-module__5TXV2W__body2.undefined") if detalhes_container else []
 
         tamanho = quartos = banheiros = vagas = None
 
         for d in detalhes:
+            title = d.get_attribute('title') or ''
             text = get_text(d)
-            if "m²" in text:
-                tamanho = self._parse_int(text)
-            elif "Quarto" in text:
-                quartos = self._parse_int(text)
-            elif "Banheiro" in text:
-                banheiros = self._parse_int(text)
-            elif "Garagem" in text:
-                vagas = self._parse_int(text)
+
+            if "Área" in title:
+                tamanho = self._parse_int(title)
+            elif "Quarto" in title or "Quartos" in title:
+                quartos = self._parse_int(title)
+            elif "Banheiro" in title or "Banheiros" in title:
+                banheiros = self._parse_int(title)
+            elif "Garagem" in title or "Garagens" in title:
+                vagas = self._parse_int(title)
+
 
         rua = get_text(ad.query_selector('p[title*=","]'))
         bairro_el = ad.query_selector_all('p[title]')
